17f47a796fa6b37be191331484997b3b05b86559
alef <alef@whoisdrwho.net>
2016-03-07 18:45:22 +0300

    first prebuild

--- test/test-cases/badEdn/input.js
+++ test/test-cases/badEdn/input.js
@@ -1,4 +1,4 @@
 
-ar q1 = Datalog`[ :find (sum ?heads) (min ?heads) (max ?heads) (count ?heads) (count-distinct ?heads)
+var q1 = Datalog`[ :find (sum ?heads) (min ?heads) (max ?heads) (count ?heads) (count-distinct ?heads)
                :with ?monster
                :in   [[?monster ?heads]]`;
